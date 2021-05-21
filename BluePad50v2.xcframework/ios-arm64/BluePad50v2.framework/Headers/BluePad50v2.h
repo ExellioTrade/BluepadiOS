dd1501494095c0be20788268c6e2792e0bc418bf
Vashchenya.Volodymyr <Vashchenya.Volodymyr@gmail.com>
2021-05-21 14:53:49 +0300

    add xcframework

---
+++ BluePad50v2.xcframework/ios-arm64/BluePad50v2.framework/Headers/BluePad50v2.h
@@ -0,0 +1,17 @@
+//
+//  BluePad50v2.h
+//  BluePad50v2
+//
+//  Created by Вова Ващеня on 14.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+//! Project version number for BluePad50v2.
+FOUNDATION_EXPORT double BluePad50v2VersionNumber;
+
+//! Project version string for BluePad50v2.
+FOUNDATION_EXPORT const unsigned char BluePad50v2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BluePad50v2/PublicHeader.h>
+#import "Headers.h"
